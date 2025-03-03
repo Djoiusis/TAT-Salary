6b2e5e631f986a502c81b48994f90cad295d2360
Djoiusis <jihaadbenyedder@gmail.com>
2025-03-03 12:18:47 +0100

    Update salary-tat.py

--- salary-tat.py
+++ salary-tat.py
@@ -76,7 +76,7 @@ def calculer_salaire_net(salaire_brut, age, situation_familiale, lpp_data, impot
 
     taux_lpp = matching_rows.iloc[0, 3]
     cotisation_lpp = salaire_brut * taux_lpp
-    st.write(type(cotisation_lpp), cotisation_lpp)
+    st.write(type(cotisation_lpp))
 
     
     colonne_impot = correspondance_situation.get(situation_familiale)
